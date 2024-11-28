79562f1bc83979726fd684f0e938a6dc05aad141
MohamedRagabb <mohamedbksaftawy@gmail.com>
2024-11-28 21:57:17 +0200

    update

--- JS/master.js
+++ JS/master.js
@@ -17,8 +17,10 @@ function SaveCateogry(){
     }
     categoryAry.push(objectcate);
     localStorage.setItem('category', JSON.stringify(categoryAry))
-    Rest()
-    ShowCateory()
+    Rest();
+    ShowCateory();
+    ShowTableCategory();
+    countcate();
     
 }
 
@@ -39,8 +41,64 @@ function ShowCateory(){
     ddlcate.innerHTML=item;
 }
 
+//showtable
+function ShowTableCategory(){
+    let Table ='';
+    for (i=0 ; i<categoryAry.length ; i++)
+        {
+            Table+=`
+              <tr>
+                     <td>${i+1}</td>
+                     <td>${categoryAry[i].category}</td>
+                                
+                     <td>
+                      
+                        <button class="btn btn-danger ms-4 " onclick = "DeleteCategory(${i})" >
+                            <i class="fa-solid fa-trash-can"></i>
+                        </button>
+                        </td>
+                        </tr>
+            `
+        }
+        document.getElementById('bodycate').innerHTML= Table;
+        
+}
+
+//deletecategory
+
+function DeleteCategory(id){
+    if(confirm('Are you sure From deleted..?')==true){
+        categoryAry.splice(id,1)
+        localStorage.category= JSON.stringify(categoryAry);
+        ShowTableCategory();
+        ShowCateory();
+        countcate();
+    }
+    
+    
+}
 
 
+//count cate
+
+function countcate(){
+    document.getElementById('countcategory').innerHTML=`--Total(${categoryAry.length})`
+}
+
+
+//validation category 
+
+function validationcategory(){
+    let valid =true;
+    if(category.value ==''){
+        alert('Enter Category Name')
+        valid=false;
+    }else{
+        SaveCateogry();
+        valid=true;
+    }
+    return valid;
+} 
 
 
 
@@ -58,6 +116,8 @@ function ShowCateory(){
 
 $(document).ready(function(){
     ShowCateory();
+    ShowTableCategory();
+    countcate();
     $('#tablepro').DataTable();
 })
  
